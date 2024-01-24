a7c1db97d8a985c1b0b91373f285778ca31ccdb4
Bar Westfried <bar.westfried@post.runi.ac.il>
2024-01-24 12:24:00 +0200

    Fixed Gen3 again

--- Gen3.java
+++ Gen3.java
@@ -1,13 +1,18 @@
 public class Gen3 {
     public static void main(String[] args) {
-        double min = double.parseDouble(args[0]);
-		double max = double.parseDouble(args[1]);
+        double min = Double.parseDouble(args[0]);
+		double max = Double.parseDouble(args[1]);
+		
 		double range = max - min;
-		int rand1 = (int)(Math.random() * range) + min;
+		
+		int rand1 = (int)((Math.random() * range) + min);
+		
 		System.out.println(rand1);
-		int rand2 = (int)(Math.random() * range) + min;
+		int rand2 = (int)((Math.random() * range) + min);
+		
 		System.out.println(rand2);
-		int rand3 = (int)(Math.random() * range) + min;
+		int rand3 = (int)((Math.random() * range) + min);
+		
 		System.out.println(rand3);
         System.out.println("The minimal generated number was " + Math.min(rand1, Math.min(rand2,rand3)));
     }
