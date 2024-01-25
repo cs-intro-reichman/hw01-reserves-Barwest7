f6bb8a1a40e481a051ce5abe195f9ccb712128de
Bar Westfried <bar.westfried@post.runi.ac.il>
2024-01-25 08:31:50 +0200

    Another fix

--- Gen3.java
+++ Gen3.java
@@ -2,12 +2,10 @@ public class Gen3 {
     public static void main(String[] args) {
         int min = Integer.parseInt(args[0]);
 		int max = Integer.parseInt(args[1]);
-		
-		int range = max - min;
-		
-		int rand1 = (int)(min + Math.random() * range);
-		int rand2 = (int)(min + Math.random() * range);
-		int rand3 = (int)(min + Math.random() * range);
+				
+		int rand1 = (int)(min + Math.random() * (max-min));
+		int rand2 = (int)(min + Math.random() * (max-min));
+		int rand3 = (int)(min + Math.random() * (max-min));
 		
 		System.out.println(rand1);
 		System.out.println(rand2);
