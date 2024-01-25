ccea76768376e84977d61841c8d682e66758b0a3
Bar Westfried <bar.westfried@post.runi.ac.il>
2024-01-25 08:44:30 +0200

    Used the right file

--- GenThree.java
+++ GenThree.java
@@ -4,6 +4,17 @@
  */
 public class GenThree {
 	public static void main(String[] args) {
-		// Put your code here	
+	    int a = Integer.parseInt(args[0]);
+		int b = Integer.parseInt(args[1]);
+
+		int rand1 = (int)(a + Math.random() * (b-a));
+		int rand2 = (int)(a + Math.random() * (b-a));
+		int rand3 = (int)(a + Math.random() * (b-a));
+		
+		System.out.println(rand1);
+		System.out.println(rand2);
+		System.out.println(rand3);
+		
+        System.out.println("The minimal generated number was " + Math.min(rand1, Math.min(rand2,rand3)));
 	}
 }
